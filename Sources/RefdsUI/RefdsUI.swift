ef6ab990faad59283bcf2a3b54f366591a423679
Rafael Escaleira <rafael.escaleira@g.globo>
2022-12-19 02:34:42 -0400

    Add title navigation bar config attributes

--- Sources/RefdsUI/RefdsUI.swift
+++ Sources/RefdsUI/RefdsUI.swift
@@ -5,7 +5,8 @@ public struct RefdsUI {
     
     @State public var defaultFontFamily: RefdsFontFamily = .moderat
     
-    public func setLargeTitleAppearance() {
+    public func setNavigationBarAppearance() {
         UINavigationBar.appearance().largeTitleTextAttributes = [.font: UIFont.refds(size: .large, weight: .bold, family: defaultFontFamily).withSize(32)]
+        UINavigationBar.appearance().titleTextAttributes = [.font: UIFont.refds(size: .normal, weight: .bold, family: defaultFontFamily).withSize(17)]
     }
 }
