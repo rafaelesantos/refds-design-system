ed798195f0bb7f8f0dd74694d4ed3fd78550dcbf
Rafael Escaleira <rafael.escaleira@g.globo>
2022-12-19 11:04:52 -0400

    Fix ui font just to iOS

--- Sources/RefdsUI/RefdsUI.swift
+++ Sources/RefdsUI/RefdsUI.swift
@@ -3,10 +3,12 @@ import SwiftUI
 public struct RefdsUI {
     public static let shared = RefdsUI()
     
-    @State public var defaultFontFamily: RefdsFontFamily = .moderat
+    public var defaultFontFamily: RefdsFontFamily = .moderat
     
+    #if os(iOS)
     public func setNavigationBarAppearance() {
         UINavigationBar.appearance().largeTitleTextAttributes = [.font: UIFont.refds(size: .large, weight: .bold, family: defaultFontFamily).withSize(32)]
         UINavigationBar.appearance().titleTextAttributes = [.font: UIFont.refds(size: .normal, weight: .bold, family: defaultFontFamily).withSize(17)]
     }
+    #endif
 }
