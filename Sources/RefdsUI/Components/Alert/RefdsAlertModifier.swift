2aea61d16ca8c26cf44d7bcc5d951087d36c72ab
Rafael Escaleira <rafael.escaleira@g.globo>
2022-12-20 16:10:12 -0400

    Create reads alert

---
+++ Sources/RefdsUI/Components/Alert/RefdsAlertModifier.swift
@@ -0,0 +1,62 @@
+//
+//  RefdsAlertModifier.swift
+//  
+//
+//  Created by Rafael Santos on 20/12/22.
+//
+
+import Foundation
+import SwiftUI
+
+struct RefdsAlertModifier {
+    @Binding private var isPresented: Bool
+
+    private let title: String
+    private let message: String
+    private let dismissButton: RefdsAlertButton?
+    private let primaryButton: RefdsAlertButton?
+    private let secondaryButton: RefdsAlertButton?
+}
+
+
+extension RefdsAlertModifier: ViewModifier {
+    func body(content: Content) -> some View {
+        content.fullScreenCover(isPresented: $isPresented) {
+            RefdsAlert(title: title, message: message, dismissButton: dismissButton, primaryButton: primaryButton, secondaryButton: secondaryButton)
+        }
+    }
+}
+
+extension RefdsAlertModifier {
+    init(title: String = "", message: String = "", dismissButton: RefdsAlertButton, isPresented: Binding<Bool>) {
+        self.title = title
+        self.message = message
+        self.dismissButton = dismissButton
+        self.primaryButton = nil
+        self.secondaryButton = nil
+        _isPresented = isPresented
+    }
+
+    init(title: String = "", message: String = "", primaryButton: RefdsAlertButton, secondaryButton: RefdsAlertButton, isPresented: Binding<Bool>) {
+        self.title = title
+        self.message = message
+        self.primaryButton = primaryButton
+        self.secondaryButton = secondaryButton
+        self.dismissButton = nil
+        _isPresented = isPresented
+    }
+}
+
+extension View {
+    func refdsAlert(title: String, message: String, dismissButton: RefdsAlertButton = RefdsAlertButton("Ok"), isPresented: Binding<Bool>) -> some View {
+        let title = NSLocalizedString(title, comment: "")
+        let message = NSLocalizedString(message, comment: "")
+        return modifier(RefdsAlertModifier(title: title, message: message, dismissButton: dismissButton, isPresented: isPresented))
+    }
+    
+    func refdsAlert(title: String, message: String, primaryButton: RefdsAlertButton, secondaryButton: RefdsAlertButton, isPresented: Binding<Bool>) -> some View {
+        let title = NSLocalizedString(title, comment: "")
+        let message = NSLocalizedString(message, comment: "")
+        return modifier(RefdsAlertModifier(title: title, message: message, primaryButton: primaryButton, secondaryButton: secondaryButton, isPresented: isPresented))
+    }
+}
