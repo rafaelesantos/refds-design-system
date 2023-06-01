a3c380df7e06bf56349da78575a106bb5701c6b4
Rafael Escaleira <rafaelescaleira@icloud.com>
2023-06-01 15:33:59 -0400

    Add parallax effect

--- Sources/RefdsUI/Foundation/Effect/RefdsParallax.swift
+++ Sources/RefdsUI/Foundation/Effect/RefdsParallax.swift
@@ -1,8 +1,45 @@
-//
-//  File.swift
-//  
-//
-//  Created by Rafael Santos on 01/06/23.
-//
+import SwiftUI
+import CoreMotion
 
-import Foundation
+struct ParallaxMotionModifier: ViewModifier {
+    @StateObject private var manager: MotionManager = MotionManager()
+    private var magnitude: Double
+    
+    init(magnitude: Double) {
+        self.magnitude = magnitude
+    }
+    
+    func body(content: Content) -> some View {
+        content
+            .offset(x: CGFloat(manager.roll * magnitude), y: CGFloat(manager.pitch * magnitude))
+    }
+}
+
+public extension View {
+    func refdsParallax(magnitude: Double) -> some View {
+        self.modifier(ParallaxMotionModifier(magnitude: magnitude))
+    }
+}
+
+class MotionManager: ObservableObject {
+    @Published var pitch: Double = 0.0
+    @Published var roll: Double = 0.0
+    
+    private var manager: CMMotionManager
+
+    init() {
+        self.manager = CMMotionManager()
+        self.manager.deviceMotionUpdateInterval = 1/60
+        self.manager.startDeviceMotionUpdates(to: .main) { (motionData, error) in
+            guard error == nil else {
+                print(error!)
+                return
+            }
+
+            if let motionData = motionData {
+                self.pitch = motionData.attitude.pitch
+                self.roll = motionData.attitude.roll
+            }
+        }
+    }
+}
