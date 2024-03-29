9a5520782b31ee18966befd1775fc64b7c88d396
Rafael Escaleira <rafaelescaleira@icloud.com>
2024-03-29 19:51:17 -0400

    Create RefdsStarShowerEdge.swift

---
+++ Sources/RefdsUI/Components/StarShower/RefdsStarShowerEdge.swift
@@ -0,0 +1,15 @@
+//
+//  File.swift
+//  
+//
+//  Created by Rafael Santos on 29/03/24.
+//
+
+import SwiftUI
+
+public enum RefdsStarShowerEdge {
+    case top
+    case bottom
+    case leading
+    case trailing
+}
