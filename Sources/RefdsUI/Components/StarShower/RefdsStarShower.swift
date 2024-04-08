4f1da62725e2d4c68df1f4cce30dd6bd1d5188a3
Rafael Escaleira <rafaelescaleira@icloud.com>
2024-04-08 10:23:51 -0400

    Update RefdsStarShower.swift

--- Sources/RefdsUI/Components/StarShower/RefdsStarShower.swift
+++ Sources/RefdsUI/Components/StarShower/RefdsStarShower.swift
@@ -6,13 +6,12 @@ public struct RefdsStarShower: View {
     
     private let timer = Timer.publish(every: 0.05, on: .current, in: .common).autoconnect()
     private let edge: RefdsStarShowerEdge
-    private let galaxyWidth: CGFloat = UIScreen.main.bounds.width
     private let galaxyHeight: CGFloat
     private let backgroundColor: Color?
     
     public init(
         from edge: RefdsStarShowerEdge = .leading,
-        galaxyHeight: CGFloat = UIScreen.main.bounds.height,
+        galaxyHeight: CGFloat = 1000,
         backgroundColor: Color? = nil
     ) {
         self.edge = edge
@@ -21,27 +20,29 @@ public struct RefdsStarShower: View {
     }
     
     public var body: some View {
-        Canvas { context, size in
-            for star in starsViewData {
-                let newContext = context
-                let rect = CGRect(x: star.position.x, y: star.position.y, width: star.size, height: star.size)
-                newContext.fill(Path(ellipseIn: rect), with: .color(star.color))
+        GeometryReader { proxy in
+            Canvas { context, size in
+                for star in starsViewData {
+                    let newContext = context
+                    let rect = CGRect(x: star.position.x, y: star.position.y, width: star.size, height: star.size)
+                    newContext.fill(Path(ellipseIn: rect), with: .color(star.color))
+                }
             }
+            .if(backgroundColor, transform: { view, color in
+                view.background(color.opacity(0.2))
+            })
+            .if(backgroundColor == nil, transform: { view in
+                view.refdsBackground(with: .secondaryBackground)
+            })
+            .onAppear { makeStars(width: proxy.size.width) }
+            .onReceive(timer) { _ in moveStars(width: proxy.size.width) }
+            .frame(height: galaxyHeight)
         }
-        .if(backgroundColor, transform: { view, color in
-            view.background(color.opacity(0.2))
-        })
-        .if(backgroundColor == nil, transform: { view in
-            view.refdsBackground(with: .secondaryBackground)
-        })
-        .onAppear { makeStars() }
-        .onReceive(timer) { _ in moveStars() }
-        .frame(height: galaxyHeight)
     }
     
-    private func makeStars() {
+    private func makeStars(width: CGFloat) {
         for _ in 1 ... 200 {
-            let position = CGPoint(x: .random(in: 0 ... galaxyWidth), y: .random(in: 0 ... galaxyHeight))
+            let position = CGPoint(x: .random(in: 0 ... width), y: .random(in: 0 ... galaxyHeight))
             let star = RefdsStarViewData(
                 size: .random(in: 2 ... 5),
                 position: position,
@@ -52,7 +53,7 @@ public struct RefdsStarShower: View {
         }
     }
     
-    private func moveStars() {
+    private func moveStars(width: CGFloat) {
         for index in starsViewData.indices {
             let star = starsViewData[index]
             switch edge {
@@ -60,7 +61,7 @@ public struct RefdsStarShower: View {
                 starsViewData[index].position.y += star.velocity
                 if star.position.y > galaxyHeight {
                     starsViewData[index].position = .init(
-                        x: .random(in: 0 ... galaxyWidth),
+                        x: .random(in: 0 ... width),
                         y: .random(in: 0 ... galaxyHeight / 100)
                     )
                 }
@@ -69,16 +70,16 @@ public struct RefdsStarShower: View {
                 starsViewData[index].position.y -= star.velocity
                 if star.position.y < 0 {
                     starsViewData[index].position = .init(
-                        x: .random(in: 0 ... galaxyWidth),
+                        x: .random(in: 0 ... width),
                         y: .random(in: galaxyHeight / 1.1 ... galaxyHeight)
                     )
                 }
                 
             case .leading:
                 starsViewData[index].position.x += star.velocity
-                if star.position.x > galaxyWidth {
+                if star.position.x > width {
                     starsViewData[index].position = .init(
-                        x: .random(in: 0 ... galaxyWidth / 100),
+                        x: .random(in: 0 ... width / 100),
                         y: .random(in: 0 ... galaxyHeight)
                     )
                 }
@@ -87,7 +88,7 @@ public struct RefdsStarShower: View {
                 starsViewData[index].position.x -= star.velocity
                 if star.position.x < 0 {
                     starsViewData[index].position = .init(
-                        x: .random(in: galaxyWidth / 1.1 ... galaxyWidth),
+                        x: .random(in: width / 1.1 ... width),
                         y: .random(in: 0 ... galaxyHeight)
                     )
                 }
