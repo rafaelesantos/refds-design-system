1bc8212092501bbeef3b014fb2adcf86892768ed
Rafael Escaleira <rafaelescaleira@icloud.com>
2024-04-15 15:34:59 -0400

    Create RefdsList.swift

---
+++ Sources/RefdsUI/Components/List/RefdsList.swift
@@ -0,0 +1,27 @@
+import SwiftUI
+
+public struct RefdsList<Content: View>: View {
+    private let content: () -> Content
+    
+    public init(@ViewBuilder content: @escaping () -> Content) {
+        self.content = content
+    }
+    
+    public var body: some View {
+        #if os(macOS)
+        ScrollView {
+            content()
+        }
+        #else
+        List {
+            content()
+        }
+        #endif
+    }
+}
+
+#Preview {
+    RefdsList {
+        RefdsText(.someWord())
+    }
+}
