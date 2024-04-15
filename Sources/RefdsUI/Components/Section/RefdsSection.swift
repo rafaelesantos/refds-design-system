462516ec35400268d4dc2239296fa16a51e2ab18
Rafael Escaleira <rafaelescaleira@icloud.com>
2024-04-15 15:12:43 -0400

    Create RefdsSection.swift

---
+++ Sources/RefdsUI/Components/Section/RefdsSection.swift
@@ -0,0 +1,60 @@
+import SwiftUI
+
+public struct RefdsSection: View {
+    private let content: () -> any View
+    private let header: () -> any View
+    private let footer: () -> any View
+    
+    public init(
+        @ViewBuilder content: @escaping () -> some View,
+        @ViewBuilder header: @escaping () -> some View = { EmptyView() },
+        @ViewBuilder footer: @escaping () -> some View = { EmptyView() }
+    ) {
+        self.content = content
+        self.header = header
+        self.footer = footer
+    }
+    
+    @ViewBuilder
+    public var body: some View {
+        #if os(iOS)
+        Section {
+           AnyView(content())
+        } header: {
+            AnyView(header())
+        } footer: {
+            AnyView(footer())
+        }
+        #else
+        VStack(alignment: .leading) {
+            AnyView(header())
+                .textCase(.uppercase)
+                .padding(.horizontal)
+            GroupBox {
+                AnyView(content())
+                    .frame(maxWidth: .infinity)
+            }
+            AnyView(footer())
+                .padding(.horizontal)
+        }
+        .padding()
+        #endif
+    }
+}
+
+#Preview {
+    ScrollView {
+        RefdsSection {
+            RefdsText(.someParagraph())
+        } header: {
+            RefdsText(.someWord(), style: .footnote, color: .secondary)
+        }
+        
+        RefdsSection {
+            RefdsText(.someParagraph())
+        } header: {
+            RefdsText(.someWord(), style: .footnote, color: .secondary)
+        }
+    }
+    .listStyle(.plain)
+}
